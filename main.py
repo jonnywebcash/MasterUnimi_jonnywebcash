c37d770544df37190fbd37930ecc8f33d99b66e4
jonnywebcash <jonnywebcash@hotmail.com>
2023-04-12 08:36:27 +0200

    Jupiter kmean
    Kmean transpose to Jupiter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import support_function as sf
 import pandas as pd
 import os
+# colnames = ['nome animale', 'pelo','piume','uova','latte','aereo','acquatico','predatore','dentato','spina dorsale','respira','velenoso ','pinne','zampe','coda','domestico','tagliagatto','tipo']
 colnames = ['animal name', 'hair','feathers','eggs','milk','airborne','aquatic','predator','toothed','backbone','breathes','venomous','fins','legs','tail','domestic','catsize','type']
 zoo_data = pd.read_csv(os.getcwd()+'/Dataset/zoo.data', delimiter=',', names=colnames, header=None)
 zoo_data['type']=zoo_data['type']-1
@@ -10,47 +11,56 @@ print(zoo_data)
 species_dict =dict(list(enumerate(['mammiferi', 'uccelli', 'rettili', 'pesci', 'anfibi', 'insetti','invertebrati'])))
 print(species_dict)
 
-import seaborn as sns
-#plotting the heatmap for correlation
-plot_heatmap = sns.heatmap(zoo_data.corr())
+
+# Plotting the heatmap for correlation between features and class
+sf.feature_vs_class_heatmap(zoo_data,species_dict)
+
+# Dataset composition analysis
+sf.class_distributuion_barplot(zoo_data,species_dict)
+sf.barplot_class_feature_distribution(zoo_data,species_dict)
 
 
 # k-means algorithm
-zoo_data=sf.kmeans_prediction(zoo_data)
+kmeans=sf.kmeans_prediction(zoo_data,7)
 # Mostra le prime 20 righe del dataframe con la colonna Cluster aggiunta
-zoo_data.head(20)
+kmeans.head(20)
+
+# DBSCAN
+dbscan=sf.dbscan_prediction(zoo_data)
+
 
 ## Valutazione cluster
 # Labelize result
-result_kmeans=sf.replace_species(zoo_data[['animal name','type','predict']],species_dict)
+result_kmeans=zoo_data[['animal name','type','predict']].copy()
+result_kmeans=sf.replace_species(result_kmeans,species_dict)
 
 # Classification performance
-sf.classification_percentage_performance(zoo_data,species_dict)
-# Similarity index
-similarity_kmeans=sf.similarity_index(zoo_data[['animal name','type','predict']])
+sf.classification_percentage_performance(result_kmeans,species_dict)
+ # Similarity index
+similarity_kmeans=sf.similarity_index(result_kmeans)
 # Dissimilarità index
-dissimilarity_kmeans=sf.dissimilarity_index(zoo_data[['animal name','type','predict']])
+dissimilarity_kmeans=sf.dissimilarity_index(result_kmeans)
 
 # Adjusted Rand Index
 #The adjusted rand index is an evaluation metric that is used to measure the similarity between two clustering by considering all the pairs of the n_samples and calculating the counting pairs of the assigned in the same or different clusters in the actual and predicted clustering.  
 #The adjusted rand index score is defined as:
 #ARI = (RI - Expected_RI) / (max(RI) - Expected_RI)
 # A score above 0.7 is considered to be a good match. 
-ari_kmeans=sf.adjusted_rand_index(zoo_data[['animal name','type','predict']])
+ari_kmeans=sf.adjusted_rand_index(result_kmeans)
 
 #Rand Index
 #The Rand index is different from the adjusted rand index. Rand index does find the similarity between two clustering by considering all the pairs of the n_sample but it ranges from 0 to 1. whereas ARI ranges from -1 to 1. 
 #The rand index is defined as:
 # RI = (number of agreeing pairs) / (number of pairs)
-ri_kmeans=sf.rand_index(zoo_data[['animal name','type','predict']])
+ri_kmeans=sf.rand_index(result_kmeans)
 
 # Silhouette Score aka Silhouette Coefficient
 # Silhouette score aka Silhouette Coefficient is an evaluation metric that results in the range of -1 to 1. A score near 1 signifies the best importance that the data point is very compact within the cluster to which it belongs and far away from the other clusters. The score near -1 signifies the least or worst importance of the data point. A score near 0 signifies overlapping clusters. 
-ss_kmeans=sf.silhouette_score_index(zoo_data)
+ss_kmeans=sf.silhouette_score_index(kmeans)
 
 # Scatter plot
-scatter_df=sf.scatter_plot_result(zoo_data[['animal name','type','predict']])     
+scatter_df=sf.scatter_plot_result(kmeans[['animal name','type','predict']])     
       
 # bar plot of predicted feature class composition
 # sf.barplot_class_feature_percentage(zoo_data,species_dict)
-sf.barplot_class_feature_comaprison(zoo_data,species_dict)
+sf.barplot_class_feature_comaprison(kmeans,species_dict)
--- support_function.py
+++ support_function.py
@@ -1,4 +1,5 @@
 import sklearn
+from sklearn.cluster import DBSCAN
 from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score
 from sklearn.metrics import rand_score
@@ -27,13 +28,21 @@ def replace_predict_with_major(df):
 
  
 def replace_species(df,species_dict):
-    df=replace_predict_with_major(df)
+    df=replace_predict_with_major(df).copy()
     df['predict']=df['predict'].map(species_dict)  
     df['type']=df['type'].map(species_dict)    
     # for chiave in species_dict.keys():
     #     df['type'] = df['type'].replace([int(chiave)], species_dict[chiave])
         #sub_df_frequency = sub_df['type'].value_counts()
     return(df)
+
+def feature_vs_class_heatmap(df,species_dict):
+    df_tmp=df.copy()
+    df_tmp['type']=df_tmp['type'].map(species_dict)    
+    # One-hot encoding
+    df_tmp=pd.get_dummies(df_tmp,columns=["type"])
+    sns.heatmap(df_tmp.corr())
+
 
 def scatter_plot_result(df):
     # df_tmp = pd.DataFrame(columns = list(df.columns.values))
@@ -45,7 +54,7 @@ def scatter_plot_result(df):
     #     df_tmp=pd.concat([df_tmp, sub_df], axis=0)
 
     # df=df_tmp
-    df=replace_predict_with_major(df)
+    df=replace_predict_with_major(df).copy()
     df['type'] = df['type'].apply(lambda x: x + np.random.randint(-40,40)/100)
     df['predict'] = df['predict'].apply(lambda x: x + np.random.randint(-40,40)/100)
     # Crea un grafico a dispersione dei dati utilizzando le colonne Età e Voto
@@ -75,16 +84,54 @@ def scatter_plot_result(df):
     plt.show()
     return(df)
 
+def class_distributuion_barplot(df,species_dict):
+    sub_df=df.groupby(["type"])["type"].count().reset_index(name="count").copy()
+    sub_df['perc'] = sub_df['count'].groupby(sub_df['type']).transform(lambda x: x/df.shape[0])
+    sub_df['type']=sub_df['type'].map(species_dict)
+    sns.set() 
+    fig, axes = plt.subplots(2, 1)
+    fig.suptitle('Dataset composition')
+    sns.barplot(x ="type", y = 'perc', data = sub_df, hue = "type",ax=axes[0])    
+    sns.barplot(x ="type", y = 'count', data = sub_df, hue = "type",ax=axes[1])
+
+def barplot_class_feature_distribution(df,species_dict):
+   feature_name=['hair','feathers','eggs','milk','airborne','aquatic','predator','toothed']
+   cnt=0
+   sns.set() 
+   fig, axes = plt.subplots(2, 4, sharey=True)
+   fig.suptitle('Class features composition')
+   for feature in feature_name:
+       sub_df=df.groupby(["type", feature])[feature].count().reset_index(name="count").copy()
+       sub_df['perc'] = sub_df['count'].groupby(sub_df['type']).transform(lambda x: x/x.sum())
+       sub_df['type']=sub_df['type'].map(species_dict) 
+       sns.barplot(x ="type", y = 'perc', data = sub_df, hue = feature,ax=axes[cnt//4,cnt%4])
+       axes[cnt//4,cnt%4].set_xticklabels(axes[cnt//4,cnt%4].get_xticklabels(), rotation=45)
+       # axes[cnt//4,cnt%4].set_title(feature)       
+       cnt=cnt+1
+    
+   feature_name=['backbone','breathes','venomous','fins','legs','tail','domestic','catsize']
+   cnt=0
+   fig, axes = plt.subplots(2, 4, sharey=True)
+   fig.suptitle('Class features composition')
+   for feature in feature_name:
+        sub_df=df.groupby(["type", feature])[feature].count().reset_index(name="count").copy()
+        sub_df['perc'] = sub_df['count'].groupby(sub_df['type']).transform(lambda x: x/x.sum())
+        sub_df['type']=sub_df['type'].map(species_dict) 
+        sns.barplot(x ="type", y = 'perc', data = sub_df, hue = feature,ax=axes[cnt//4,cnt%4])
+        axes[cnt//4,cnt%4].set_xticklabels(axes[cnt//4,cnt%4].get_xticklabels(), rotation=45)        
+        # axes[cnt//4,cnt%4].set_title(feature)        
+        cnt=cnt+1
+
 def barplot_class_feature_percentage(df,species_dict):
     feature_name=['hair','feathers','eggs','milk','airborne','aquatic','predator','toothed','backbone','breathes','venomous','fins','legs','tail','domestic','catsize','type']
     for result_predict in (df['predict'].unique()):
         
-        sub_df=df[df["predict"]==result_predict]    
+        sub_df=df[df["predict"]==result_predict].copy() 
         cnt=1
         plt.figure()
         for feature in feature_name:
             # get colum value distribution
-            a = (sub_df[feature].value_counts(normalize=True)
+            a = (sub_df[feature].value_counts(normalize=True) 
                         .mul(100)
                         .rename_axis(feature)
                         .reset_index(name='percentage'))
@@ -119,7 +166,7 @@ def barplot_class_feature_comaprison(df,species_dict):
        # axes[cnt//4,cnt%4].set_title(feature)       
        cnt=cnt+1
     
-   feature_name=['backbone','breathes','venomous','fins','legs','tail','domestic','catsize','type']
+   feature_name=['backbone','breathes','venomous','fins','legs','tail','domestic','catsize']
    cnt=0
    fig, axes = plt.subplots(2, 4, sharey=True)
    fig.suptitle('Class features composition')
@@ -133,38 +180,37 @@ def barplot_class_feature_comaprison(df,species_dict):
         cnt=cnt+1
 
 def classification_percentage_performance(df,species_dict):
-    df=replace_predict_with_major(df)    
+    df=replace_predict_with_major(df).copy()    
     sub_df=df.groupby(["type", "predict"])["predict"].count().reset_index(name="count")
     sub_df['perc'] = sub_df['count'].groupby(sub_df['type']).transform(lambda x: x/x.sum())
-    sub_df['predict']=sub_df['predict'].map(species_dict)
-    sub_df['type']=sub_df['type'].map(species_dict)
+    if   type(sub_df['predict'][0]) == int or type(sub_df['predict'][0]) == float:
+        sub_df['predict']=sub_df['predict'].map(species_dict)
+    if   type(sub_df['type'][0]) == int or type(sub_df['type'][0]) == float:
+        sub_df['type']=sub_df['type'].map(species_dict)
     sns.set() 
     fig, axes = plt.subplots(2, 1)
-    fig.suptitle('Class features composition')
+    fig.suptitle('Distribution of classified classes')
     sns.barplot(x ="type", y = 'perc', data = sub_df, hue = "predict",ax=axes[0])    
     sns.barplot(x ="type", y = 'count', data = sub_df, hue = "predict",ax=axes[1])
     
 
 def similarity_index(df):
-    # df_tmp = pd.DataFrame(columns = list(df.columns.values)+['predict label'])
-    # # Valutazione predict
-    # for result_predict in (df['predict'].unique()):
-    #     sub_df=df[df["predict"]==result_predict]
-    #     major_species=sub_df['type'].value_counts().idxmax()
-    #     sub_df['predict label'] =major_species
-    #     df_tmp=pd.concat([df_tmp, sub_df], axis=0)
-
     # df=df_tmp
     predict_major=df['type'].groupby(df['predict']).value_counts().groupby(level=[0], group_keys=False).head(1).to_frame('counts').reset_index()
     predict_major=df.set_index('predict').to_dict()['type']
-    df['predict label']=df['predict'].map(predict_major)
+    if   type(df['predict'][0]) == int or type(df['predict'][0]) == float:
+        df['predict']=df['predict'].map(predict_major)
+    
 
     df=replace_predict_with_major(df)
     similarity_dict={}
     for real_type in (df['type'].unique()):
         sub_df=df[df["type"]==real_type]
-        match=sub_df[sub_df["predict label"]==real_type]
-        similarity_dict[real_type]=match.shape[0]/sub_df.shape[0]*100
+        match=sub_df[sub_df["predict"]==real_type]
+        if match.shape[0]>0:
+            similarity_dict[real_type]=match.shape[0]/sub_df.shape[0]*100
+        else:
+            similarity_dict[real_type]=0
     myKeys = list(similarity_dict.keys())
     myKeys.sort()
     sorted_dict = {i: similarity_dict[i] for i in myKeys}
@@ -179,7 +225,7 @@ def similarity_index(df):
     return(similarity_dict)
 
 def dissimilarity_index(df):
-    df_tmp = pd.DataFrame(columns = list(df.columns.values)+['predict label'])
+    df_tmp = pd.DataFrame(columns = list(df.columns.values)).copy()
     # Valutazione predict
     for result_predict in (df['predict'].unique()):
         sub_df=df[df["predict"]==result_predict]
@@ -220,14 +266,20 @@ def silhouette_score_index(df):
 
    
 
-def kmeans_prediction(df):
+def kmeans_prediction(df, cluster_num):
     # Definiamo il numero K di cluster presenti nei dati:
     # Crea un'istanza di KMeans con 7 cluster
-    kmeans = KMeans(n_clusters=7)
+    kmeans = KMeans(n_clusters=cluster_num)
     # Addestra il modello di K-means sul dataset
-    kmeans.fit(df[['hair','feathers','eggs','milk','airborne','aquatic','predator','toothed','backbone','breathes','venomous','fins','legs','tail','domestic','catsize']])
+    features=df[['hair','feathers','eggs','milk','airborne','aquatic','predator','toothed','backbone','breathes','venomous','fins','legs','tail','domestic','catsize']].copy()
+    kmeans.fit(features)
     
     # Assegna le etichette dei cluster a ogni oggetto del dataset
-    df['predict'] = kmeans.predict(df[['hair','feathers','eggs','milk','airborne','aquatic','predator','toothed','backbone','breathes','venomous','fins','legs','tail','domestic','catsize']])
+    df['predict'] = kmeans.predict(features)
     return(df)
+
+def dbscan_prediction(df):
+    dbscan_model = DBSCAN( eps = 0.75, min_samples = 5)
+    features=df[['hair','feathers','eggs','milk','airborne','aquatic','predator','toothed','backbone','breathes','venomous','fins','legs','tail','domestic','catsize']]
+    dbscan_model.fit(features)
 
